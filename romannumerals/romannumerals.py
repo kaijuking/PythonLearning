02f9d7c07f1ffe1f464efcf06065ceed56b289e0
Michael Field <mfield@cylance.com>
2020-06-09 18:16:54 -0700

    updated v1 function, added a v2 function and cleaned up code

--- romannumerals/romannumerals.py
+++ romannumerals/romannumerals.py
@@ -1,20 +1,30 @@
-def roman_numerals(x):
-    roman_val = {"i": 1, "v": 5, "x": 10, "l": 50, "c": 100, "m": 1000}
-
+def roman_numerals_v1(x):
+    
+    # setup variables
     total = 0
     tmp_list = []
+    roman_val = get_roman_values()
+
+    # get rid of whitespaces
+    x = get_rid_of_whitespaces(x)
+
+    # return 0 if empty string
+    if len(x) == 0:
+        return 0
+
+    # setup variables for further processing
     input_length = len(x)
-    input = x.lower()
+    input_str = x.lower()
 
     # covers the case where the inputted value is only one character
     if input_length > 0 and input_length < 2:
-        for i in range(len(input)):
-            total += roman_val[input[i]]
+        for i in range(len(input_str)):
+            total += roman_val[input_str[i]]
 
     # covers the case where the inputted value is two or more characters
     if input_length > 1:
-        for i in range(len(input)):
-            tmp_list.append(roman_val[input[i]])
+        for i in range(len(input_str)):
+            tmp_list.append(roman_val[input_str[i]])
 
     # compare two indexes to see if 'i + 1' is greater than 'i'
     for i in range(len(tmp_list) - 1):
@@ -26,14 +36,63 @@ def roman_numerals(x):
     for i in tmp_list:
         total += i
 
-    print("Input = {0}, Actual Value = {1}".format(input, str(total)))
     return total
+
+
+# v2 function - (romans_str: str) -> int > this tells the user the function take a string and returns an integer
+def roman_numerals_v2(romans_str: str) -> int:
+
+    # setup variables
+    total = 0
+    roman_val = get_roman_values()
+   
+    # get rid of whitespaces
+    input_string = get_rid_of_whitespaces(romans_str)
+
+    # return 0 if empty string
+    if len(input_string) == 0:
+        return 0
+
+    # create a list of numeric values based on the input string
+    tmp_list = [roman_val[item.lower()] for item in input_string]
+    string_length = len(tmp_list)
+
+    # covers the case where the inputted value is only one character
+    if string_length == 1:
+        return roman_val[input_string.lower()]
+
+    # compare two indexes to see if 'i + 1' is greater than 'i'
+    for i in range(string_length - 1):
+        if tmp_list[i + 1] > tmp_list[i]:
+            tmp_list[i], tmp_list[i + 1] = 0, (tmp_list[i + 1] - tmp_list[i])
+        # to save time, instead of replacing the element in a temp_list, add converted values to "total"
+        total += tmp_list[i]
+        
+    return total + tmp_list[-1]  # the last el should be added as for loop range was (input_length - 1)  
+
+
+
+def get_rid_of_whitespaces(string_to_process):
+    if " " in string_to_process:
+        return string_to_process.replace(" ", "")
+    else:
+        return string_to_process
+
+
+def get_roman_values():
+    return {"i": 1, "v": 5, "x": 10, "l": 50, "c": 100, "d": 500, "m": 1000}
 
 
 if __name__ == "__main__":
     test_values = {"i": 1, "iv": 4, "lxx": 70, "MMXX": 2020, "xiv": 14, "mmxlix": 2049, "m": 1000, "MCMLIV": 1954,
-                   "MCCXXXIV": 1234, "": 0}
+                   "MCCXXXIV": 1234, "": 0, "D": 500, "   ": 0, "   i  ": 1}
 
     for k, v in test_values.items():
-        actual_value = roman_numerals(k)
+        actual_value = roman_numerals_v1(k)
+        print("V1 - String = {0}, Expected = {1}, Actual = {2}".format(k, v, actual_value))
+        assert actual_value == v, "Input = {0}, Expected Value = {1}, Actual Value {2}".format(k, v, actual_value)
+
+    for k, v in test_values.items():
+        actual_value = roman_numerals_v2(k)
+        print("V2 - String = {0}, Expected = {1}, Actual = {2}".format(k, v, actual_value))
         assert actual_value == v, "Input = {0}, Expected Value = {1}, Actual Value {2}".format(k, v, actual_value)
