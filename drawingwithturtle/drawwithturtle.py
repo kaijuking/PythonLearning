5082cfcc4f7ad344e59c0045479f8e18eec17a2e
Michael Field <kaijuking28@gmail.com>
2020-06-16 16:05:53 -0700

    updated main code and removed unusded code

--- drawingwithturtle/drawwithturtle.py
+++ drawingwithturtle/drawwithturtle.py
@@ -1,6 +1,15 @@
 import turtle
 
 
+# Instructor's solution (which makes way more sense)
+def draw_polygon(length, sides):
+    angle = 360 / sides
+    for i in range(sides):
+        turtle.fd(length)
+        turtle.lt(angle)
+
+
+# My original approach
 def draw_object():
     print("test")
     draw_square(50, 90)
@@ -22,3 +31,15 @@ def draw_triangle(length, angle):
 
 if __name__ == "__main__":
     draw_object()
+
+    polygon_list = [
+        {"length": 100, "sides": 3},
+        {"length": 100, "sides": 4},
+        {"length": 100, "sides": 6},
+        {"length": 100, "sides": 8},
+        {"length": 100, "sides": 10}
+    ]
+    for i in range(len(polygon_list)):
+        length = polygon_list[i]["length"]
+        sides = polygon_list[i]["sides"]
+        draw_polygon(length, sides)
