74251ce3f637a15459d179c43f2b5784205b0d48
Michael Field <kaijuking28@gmail.com>
2020-06-17 18:29:24 -0700

    added sys.path in order for modules to be found

--- src/myfolder/mytest.py
+++ src/myfolder/mytest.py
@@ -1,4 +1,12 @@
-from common import hello_world
+import sys
+import os
+sys.path.append(os.pardir)
+sys.path.append(os.path.join(os.pardir, os.pardir))
+
+from src.common import helpers as h
 
 def print_hello_world():
-    hello_world
+    h.hello_world()
+
+if __name__ == "__main__":
+    print_hello_world()
